9d7e19f2d3c7f0ce8d6b82f9c8593e4a96ab0895
Sarthak Bansal <Sarthakbansal2542003@gmail.com>
2025-09-07 10:11:26 +0530

    Updates

---
+++ src/js/api.js
@@ -0,0 +1,40 @@
+export let foreignStocks = [
+  { symbol: "AAPL", name: "Apple Inc." },
+  { symbol: "MSFT", name: "Microsoft Corporation" },
+  { symbol: "TSLA", name: "Tesla Inc." },
+  { symbol: "NVDA", name: "Nvidia Corporation" },
+  { symbol: "AMZN", name: "Amazon.com Inc." },
+  { symbol: "META", name: "Meta Platforms Inc." },
+  { symbol: "JPM", name: "JPMorgan Chase & Co." },
+  { symbol: "V", name: "Visa Inc." },
+  { symbol: "WMT", name: "Walmart Inc." },
+  { symbol: "ORCL", name: "Oracle Corporation" },
+  { symbol: "BAC", name: "Bank of America" },
+  { symbol: "NFLX", name: "Netflix Inc." }];
+
+export async function fetchQuote(symbol) {
+  let url=`https://global-stocks-dashboard.onrender.com`;
+  let response = await fetch(url);
+  let data = await response.json();
+  return {
+    price: data.c,
+    changePercent: ((data.c - data.pc) / data.pc) * 100
+  };
+}
+
+export async function fetchAllStocks(stocks) {
+  let promises = stocks.map(stock =>
+    fetchQuote(stock.symbol).then(quote => ({
+      symbol: stock.symbol,
+      name: stock.name,
+      price: quote.price,
+      changePercent: quote.changePercent
+    }))
+      .catch(e => {
+        console.error("Error Fetching", stock.symbol, e);
+        return null;
+      })
+  );
+  const results = await Promise.all(promises);
+  return results.filter(Boolean);
+}
